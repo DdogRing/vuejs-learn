9c48378f39963cfe1bf69d2e927983b336e8fa56
DdogRing <2423372406@qq.com>
2021-05-22 10:23:32 +0800

    2021年5月22日10:23:27

--- Vuejs02/01-webpack的使用/02-webpack的配置/src/main.js
+++ Vuejs02/01-webpack的使用/02-webpack的配置/src/main.js
@@ -1,11 +1,11 @@
 // 1、使用commonjs的模块化规范
-const {add, mul} = require('./mathUtils.js')
+const {add, mul} = require('./js/mathUtils.js')
 
 console.log(add(20, 30))
 console.log(mul(20, 30))
 
 // 2、使用ES6的模块化规范
-import {name, age, length} from "./info";
+import {name, age, length} from "./js/info";
 
 
 console.log(name)
