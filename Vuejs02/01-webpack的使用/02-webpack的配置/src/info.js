1c199cde15c843495018f7fb4737b26dd73c5a6a
DdogRing <2423372406@qq.com>
2021-05-22 10:05:14 +0800

    2021年5月22日10:05:07

---
+++ Vuejs02/01-webpack的使用/02-webpack的配置/src/info.js
@@ -0,0 +1,4 @@
+export const name = 'ddogring'
+export const age = 22
+export const length = 18
+
---
+++ Vuejs02/01-webpack的使用/02-webpack的配置/webpack.config.js
@@ -0,0 +1,11 @@
+// 动态获取path
+const path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    mode: 'development'
+}
